ae5d3037c926deb2cb6570c0c400dd9832538914
Antonio Galvan <antonio.galvan@nowports.com>
2024-07-25 02:13:44 -0600

    Adding prisma and user service, install prisma client and onfig sqlite db and db file to create database config.
    Signed-off-by: Antonio Galvan <antonio.galvan@nowports.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaService } from './prisma-service/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    envFilePath: ['.env.development.local', '.env.development', '.env'],
+  })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}
