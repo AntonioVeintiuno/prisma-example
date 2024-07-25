70020003531b737412cb08d7016275c640c528ab
Antonio Galvan <antonio.galvan@nowports.com>
2024-07-25 02:34:08 -0600

    Add post service and routes to users and post service in app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaService } from './prisma-service/prisma.service';
 import { UserService } from './user/user.service';
+import { PostService } from './post/post.service';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: ['.env.development.local', '.env.development', '.env'],
   })],
   controllers: [AppController],
-  providers: [AppService, PrismaService, UserService],
+  providers: [AppService, PrismaService, UserService, PostService],
 })
 export class AppModule {}
